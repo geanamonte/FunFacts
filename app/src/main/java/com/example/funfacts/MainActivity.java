f9ef1abaecc2b3c2898a0f8ae627f1bd5424168c
Georgeana Dias <geanamonte@gmail.com>
2019-01-24 18:10:17 -0200

    Criação de uma nova classe FactBook para organização dos FunFacts

--- app/src/main/java/com/example/funfacts/MainActivity.java
+++ app/src/main/java/com/example/funfacts/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private TextView factTextView;
     private Button showFactButton;
+    private FactBook factBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +22,10 @@ public class MainActivity extends AppCompatActivity {
         showFactButton = findViewById(R.id.ShowFactButton);
 
         View.OnClickListener listener = new View.OnClickListener() {
-            String[] facts = {
-                    "Ants stretch when they wake up in the morning.",
-                    "Ostriches can run faster than horses.",
-                    "Olympic gold medals are actually made mostly of silver.",
-                    "You are born with 300 bones; by the time you are an adult you will have 206.",
-                    "It takes about 8 minutes for light from the Sun to reach Earth.",
-                    "Some bamboo plants can grow almost a meter in just one day.",
-                    "The state of Florida is bigger than England.",
-                    "Some penguins can leap 2-3 meters out of the water.",
-                    "On average, it takes 66 days to form a new habit.",
-                    "Mammoths still walked the earth when the Great Pyramid was being built." };
+
             @Override
             public void onClick(View view){
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(facts.length);
-                String fact = facts[randomNumber];
+                String fact = factBook.getFact();
                 factTextView.setText(fact);
             }
         };
