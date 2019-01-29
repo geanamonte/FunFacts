2c9b0e3e155ad7479ee35cfbe09fa94c274598ef
Georgeana Dias <geanamonte@gmail.com>
2019-01-29 11:43:13 -0200

    Inserção de Toast Notifications e Log Messages

--- app/src/main/java/com/example/funfacts/MainActivity.java
+++ app/src/main/java/com/example/funfacts/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.funfacts;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TextView factTextView;
     private Button showFactButton;
     private FactBook factBook = new FactBook();
@@ -38,5 +41,8 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
+        Log.d(TAG,"We' re logging from onCreate() method!");
+        Toast.makeText(this,"yay!",Toast.LENGTH_SHORT).show();
+
     }
 }
